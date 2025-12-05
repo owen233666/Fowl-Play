bae88f2c2315ee2f295d3e62e82d7d0d7dc47170
aqariio <aquario.mw@gmail.com>
2025-12-05 12:55:28 -0500

    Fix changelog parsing

--- .github/scripts/parse-changelog.sh
+++ .github/scripts/parse-changelog.sh
@@ -4,7 +4,7 @@ tag=$1
 changelog=$(cat CHANGELOG.md)
 
 # Extract the changelog section for the specified tag
-list=$(echo "$changelog" | sed -n "/^## $tag/,/^## [0-9]/p" | sed -e '1d;$d')
+list=$(echo "$changelog" | sed -n "/^## $tag$/,/^## [^ ]/p" | sed -e '1d;$d')
 
 # Remove the first blank line if it exists
 list=$(echo "$list" | sed -e '1{/^$/d;}')
